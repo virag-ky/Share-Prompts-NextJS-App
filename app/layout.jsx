ca760aa849eeaf01b70a97f315ed55fa06f21d91
Virag Kormoczy <vkormoczy@gmail.com>
2023-06-01 16:26:51 +0300

    Set up project

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import '@styles/globals.css';
+
+export const metadata = {
+  title: 'Promptopia',
+  description: 'Discover & share AI Prompts'
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang='en'>
+      <body>
+        <div className='main'></div>
+        <div className='gradient' />
+        <main className='app'>
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout;
