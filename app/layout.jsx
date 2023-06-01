16f7c4cd47d3193722ab39b2b2f1302f8a9f4990
Virag Kormoczy <vkormoczy@gmail.com>
2023-06-01 22:31:05 +0300

    Set up NextAuth

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,25 +1,27 @@
-import '@styles/globals.css';
-import Nav from '@components/Nav';
-import Provider from '@components/Provider';
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-  title: 'Promptopia',
-  description: 'Discover & share AI Prompts'
-}
+  title: "Promptopia",
+  description: "Discover & share AI Prompts",
+};
 
 const RootLayout = ({ children }) => {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body>
-        <div className='main'></div>
-        <div className='gradient' />
-        <main className='app sm:px-16 px-6'>
-          <Nav />
-          {children}
-        </main>
+        <Provider>
+          <div className="main"></div>
+          <div className="gradient" />
+          <main className="app sm:px-16 px-6">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
-  )
-}
+  );
+};
 
 export default RootLayout;
