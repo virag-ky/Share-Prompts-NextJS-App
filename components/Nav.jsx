b6400efa83eeda58bf6aec2c75ebfbccaaff1eb0
Virag Kormoczy <vkormoczy@gmail.com>
2023-06-01 17:58:54 +0300

    Set up getProviders function for sign in

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,16 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
   const isUserLoggedIn = true;
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setTheProviders = async () => {
+      const response = await getProviders();
+      setProviders(response)
+    }
+    setTheProviders()
+  }, [])
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link className="flex gap-2 felx-center" href='/'>
@@ -17,7 +27,15 @@ const Nav = () => {
       <div className="sm:flex hidden">
         {isUserLoggedIn ? (<div className="flex gap-3 md:gap-5">
           <Link href='/create-prompt' className="black_btn hover:bg-white hover:text-black">Create Post</Link>
-        </div>) : (<></>)}
+          <button type="button" onClick={signOut} className="outline_btn hover:bg-black hover:text-white ">Sign Out</button>
+          <Link href='/profile'>
+            <Image src='/assets/images/logo.svg' className="rounded-full" alt="profile" width={37} height={37}></Image>
+          </Link>
+        </div>) : (<>
+          {providers && Object.values(providers).map((provider) => (
+            <button type='button' key={provider.name} onClick={() => signIn(provider.id)} className="black_btn">Sign In</button>
+          ))}
+        </>)}
       </div>
     </nav >
   )
