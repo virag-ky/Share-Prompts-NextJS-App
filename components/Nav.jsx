9fb0a883bc8a82b3c7879e7313912e23373a3ffa
Virag Kormoczy <vkormoczy@gmail.com>
2023-06-02 20:00:06 +0300

    Add user authentication

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
@@ -30,7 +30,7 @@ const Nav = () => {
         <p className="logo_text max-sm:hidden">Promptopia</p>
       </Link>
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link
               href="/create-prompt"
@@ -74,7 +74,7 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
               src="/assets/images/logo.svg"
