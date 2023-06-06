ab0c35384571ed169f368d6823c33c78f0f154a2
Virag Kormoczy <vkormoczy@gmail.com>
2023-06-06 13:58:06 +0300

    Add solution for cache problem

--- next.config.js
+++ next.config.js
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["mongoose"],
-  },
-  images: {
-    domains: ['lh3.googleusercontent.com'],
-  },
-  webpack(config) {
-    config.experiments = {
-      ...config.experiments,
-      topLevelAwait: true,
-    }
-    return config
-  }
 }
 
 module.exports = nextConfig
